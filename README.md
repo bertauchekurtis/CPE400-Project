499852dff22150ac7f114e2abccdacebb21ef3de
bertauchekurtis <kurtisbertauche@gmail.com>
2022-11-21 16:13:21 -0800

    outlined the classes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CPE400-Project
+## Dynamic Routing Protocol to Minimize Energy Consumption
+### Final Project for a CPE 400 class at UNR
+
+## Input File
+The input file for the program should be a .csv file. For every router, there should be one line in the .csv file. The first value on the line should be the router, and all of the following values should be routers that the router is physically connected to (immediate neighbors). For example, if router b was connected to routers f, i, and k, the entry on that line should be: b,f,i,k .
--- project.py
+++ project.py
@@ -5,34 +5,57 @@
 # libraries
 
 class Packet:
-    def __init__(self, destination):
-        self.destination = destination   
-    visitedRouters = []
+    def __init__(self, destination, id, creationTime):
+        self.destination = destination
+        self.id = id
+        self.creationTime = creationTime
+        self.arrivalTime = -1
+        self.visitedNodes = []
+        self.source = ''
+        self.receivedPackets = []
 
 class Router:
-    def __init__(self, key, energy):
-        self.key = key
-        self.energy = energy
-        self.alive = True
-        if(energy < 0):
-            self.alive = False
-    
-    queue = []
+    def __init__(self, id, energyLevel, immediateNeighbors):
+        self.id = id
+        self.energyLevel = energyLevel
+        self.immediateNeighbors = immediateNeighbors
+        self.pathTable = {}
+        self.bufferQueue = []
 
-    def receivePacket(self, packet):
-        self.queue.append(packet)
+    def learnPaths(self, path):
+        # given a path, should add any new/updated paths to the path table
+        print("UH OH! Function \"learnPaths\" not implemented!")
+        for destination in reversed(path): # this should go backwards? then don't need the next line
+            if destination in self.pathTable:
+                # a route to this destination is already known, check if this path is shorter
+                # if so, update it, otherwise, go to next
+                print("hi")
+            else:
+                # new path known! add to dictionary
+                print("hi")
 
-    def transmitPacket(self):
-        frontPacket = self.queue[0]
-        self.queue.popleft()
-        frontPacket.visitedRouters.append(self.key)
-        energy = energy - 1
-        if(energy < 0):
-            self.alive = False
-        return frontPacket
+    def getTopPacketAndHop(self):
+        # should return two things, the next hop for the top packet and the top packet
+        # the next hop should be either found by the path table
+        # if the path table does not have the answer, randomly choose a neighbor
+        # decrement energy when packet is sent
+        print("UH OH! Function \"getTopPacketAndHop\" not implemented!")
 
-    def getNextPacketDestination(self):
-        return self.queue[0].destination
+    def receivePacket(self, newPacket, currentTime):
+        # given a new packet, the router should process the packet
+        # if router = destination:
+            # send the packet's path to the learn paths function
+            # remove packet from the sim and add to stats
+        # else
+            # send the packet's path to the learn paths function
+            # add the packet to the bufferQueue
+        self.learnPaths(newPacket.visitedNodes)
+        if(newPacket.destination == self.id):
+            newPacket.arrivalTime == currentTime   
+            self.receivedPackets.append(newPacket) 
+        else:
+            self.bufferQueue.append(newPacket)
+
 
 # begin of main
 # read in file of nodes and connections
@@ -43,6 +66,8 @@ for x in file:
     x = x.split(",")
     x[-1] = x[-1].replace('\n', "")
     globalView[x[0]] = x[1:]
+test = Router(1,2,3)
+test.learnPaths(['a','b','c','e'])
 
 print(globalView["a"])
 
