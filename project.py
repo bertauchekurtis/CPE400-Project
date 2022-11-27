8f23669392bd07ed7384909a8ddbda25eb45a9af
bertauchekurtis <kurtisbertauche@gmail.com>
2022-11-26 22:06:02 -0800

    lots of work flushing out protocol

--- project.py
+++ project.py
@@ -3,58 +3,76 @@
 # project.py
 
 # libraries
+NUM_BEST_PATHS = 3
 
 class Packet:
-    def __init__(self, destination, id, creationTime):
-        self.destination = destination
+    def __init__(self, ROUTE_REQUEST, PATH_KNOWN, pathInformation, placeInPath, sourceRouter, destinationRouter, id, creationTime):
+        self.ROUTE_REQUEST = ROUTE_REQUEST
+        self.PATH_KNOWN = PATH_KNOWN
+        self.pathInformation = pathInformation
+        self.placeInPath = placeInPath
+        self.sourceRouter = sourceRouter
+        self.destinationRouter = destinationRouter
         self.id = id
         self.creationTime = creationTime
-        self.arrivalTime = -1
-        self.visitedNodes = []
-        self.source = ''
-        self.receivedPackets = []
+        self.arrivalTime = -1 # indicates the packet has not arrived yet
+        self.hopCount = 0
+
+        # now, do some error checking
+        if(self.ROUTE_REQUEST == self.PATH_KNOWN):
+            print("ERROR! ==================================\nAttempt to set ROUTE_REQUEST equivalent to PATH_KNOWN in packet: " + str(id))
+            print("=========================================") 
+
 
 class Router:
     def __init__(self, id, energyLevel, immediateNeighbors):
         self.id = id
         self.energyLevel = energyLevel
         self.immediateNeighbors = immediateNeighbors
-        self.pathTable = {}
+        self.pathTables = []
         self.bufferQueue = []
+        self.finishedQueue = []
+
+        for x in range(NUM_BEST_PATHS):
+            self.pathTables.append(dict())
 
     def learnPaths(self, path):
-        # given a path, should add any new/updated paths to the path table
-        print("UH OH! Function \"learnPaths\" not implemented!")
-        for destination in reversed(path): # this should go backwards? then don't need the next line
-            if destination in self.pathTable:
-                # a route to this destination is already known, check if this path is shorter
-                # if so, update it, otherwise, go to next
-                print("hi")
-            else:
-                # new path known! add to dictionary
-                print("hi")
+        mutablePathList = list.copy(path)
+        for x in range(len(mutablePathList) - 1):
+            # determine all of the paths
+            destNode = mutablePathList.pop(0)
+            pathToDest = list.copy(mutablePathList)
+            list.reverse(pathToDest)
+
+            # print for debug:
+            # print("Path to node: " + destNode, end = " ")
+            # for y in range(len(pathToDest)):
+            #   print(pathToDest[y], end = ", ")
+            # print("\n")
+
+            # now, destNode holds the node for the path in pathToDest
+            currentPaths = []
+            for y in range(NUM_BEST_PATHS):
+                if self.pathTables[y].get(destNode) is None:
+                   self.pathTables[y][destNode] = pathToDest
+                   break
+                else:
+                    currentPaths.append(self.pathTables[y].get(destNode))
+            longestPath = len(currentPaths[0])
+            longestPathIndex = 0
+            for y in range(NUM_BEST_PATHS - 1):
+                if len(currentPaths[y + 1]) > longestPath:
+                    longestPath = len(currentPaths[y + 1])
+                    longestPathIndex = y + 1
+            
+
+        
 
     def getTopPacketAndHop(self):
-        # should return two things, the next hop for the top packet and the top packet
-        # the next hop should be either found by the path table
-        # if the path table does not have the answer, randomly choose a neighbor
-        # decrement energy when packet is sent
         print("UH OH! Function \"getTopPacketAndHop\" not implemented!")
 
     def receivePacket(self, newPacket, currentTime):
-        # given a new packet, the router should process the packet
-        # if router = destination:
-            # send the packet's path to the learn paths function
-            # remove packet from the sim and add to stats
-        # else
-            # send the packet's path to the learn paths function
-            # add the packet to the bufferQueue
-        self.learnPaths(newPacket.visitedNodes)
-        if(newPacket.destination == self.id):
-            newPacket.arrivalTime == currentTime   
-            self.receivedPackets.append(newPacket) 
-        else:
-            self.bufferQueue.append(newPacket)
+        print("UH OH")
 
 
 # begin of main
@@ -67,7 +85,8 @@ for x in file:
     x[-1] = x[-1].replace('\n', "")
     globalView[x[0]] = x[1:]
 test = Router(1,2,3)
-test.learnPaths(['a','b','c','e'])
+test.learnPaths(['a','b','c','d'])
+pack = Packet(True, True, 1, 2, 3, 4, 5, 6)
 
 print(globalView["a"])
 
