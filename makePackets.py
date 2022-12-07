38d8f4234d592060fc34a40241b2eb37bbe3d069
bertauchekurtis <kurtisbertauche@gmail.com>
2022-12-07 15:38:00 -0800

    added packet generator

---
+++ makePackets.py
@@ -0,0 +1,34 @@
+# kurtis bertauche
+# CPE 400 project
+# makePackets.py
+
+import random
+import sys
+
+numArguments = len(sys.argv)
+if(numArguments != 5):
+    print("Error, incorrect number of command line arguments supplied. Proper usage:")
+    print("py makePackets.py <network file> <number of packts> <max spawn time> <output file>")
+    exit()
+
+NETWORK_FILE = sys.argv[1]
+NUM_PACKETS = int(sys.argv[2])
+MAX_SPAWN_TIME = int(sys.argv[3])
+OUT_FILE = sys.argv[4]
+
+networkFile = open(NETWORK_FILE, "r")
+routerList = []
+
+for router in networkFile:
+    router = router.split(",")
+    routerList.append(router[0])
+
+packetFile = open(OUT_FILE, "w")
+
+for i in range(1, NUM_PACKETS + 1):
+    sourceRouter = random.choice(routerList)
+    destinationRouter = random.choice(routerList)
+    spawnTime = random.randint(0, MAX_SPAWN_TIME)
+    packetFile.write(str(spawnTime) + "," + sourceRouter + "," + destinationRouter + "\n")
+
+packetFile.close()
